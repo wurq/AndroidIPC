4aa00dd437e326dd70f08ab9b0f215a0b8284b96
wurongqiu <wurongqiu@kaike.la>
2018-03-30 16:21:25 +0800

    init base ui,base presenter, base style, define base application

---
+++ app/src/main/java/com/arch/application/IPCApplication.java
@@ -0,0 +1,14 @@
+package com.arch.application;
+
+import com.arch.base.application.BaseApplication;
+
+/**
+ * Created by wurongqiu on 2018/3/30.
+ */
+
+public class IPCApplication extends BaseApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/arch/base/application/BaseApplication.java
@@ -0,0 +1,14 @@
+package com.arch.base.application;
+
+import android.app.Application;
+
+/**
+ * Created by wurongqiu on 2018/3/30.
+ */
+
+public abstract class BaseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
